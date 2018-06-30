a7b8fb9ab97886baa6e1a1fa9fc285bfe66704e7
nainglinaung <nainglinaung91@gmail.com>
2018-06-30 14:58:52 +0630

    hardcoded

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
 let mongoose = require('mongoose');
 let process = require('process');
 
-let dbuser = process.env.dbuser;
-let dbpassword = process.env.dbpassword;
-console.log(dbuser);
-console.log(dbpassword);
+let dbuser = 'cupidMyanmar';
+let dbpassword = 'Loremipsum91';
+// console.log(dbuser);
+// console.log(dbpassword);
 mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds123981.mlab.com:23981/cupid`);
 
