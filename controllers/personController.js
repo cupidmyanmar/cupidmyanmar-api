a89471252488ed2e036af5d14c648aee74f1cffd
nainglinaung <nainglinaung91@gmail.com>
2018-06-30 14:21:54 +0630

    add post method

--- controllers/personController.js
+++ controllers/personController.js
@@ -3,8 +3,24 @@ let personModel = require('../models/person');
 let personController = {
   
 
-    post: (req,res) => {
+    // id:  {type:String, required:true},
+    // name: {type:String},
+    // age: {type:String},
+    // gender: {type:String},
+    // photo: {type:String},
+    // description: {type:String} 
 
+    post: async (req,res) => {
+        let {id,name,age,gender,photo,description} = req.body;
+        let person = await personModel.findOne({id});
+        if (person) {
+            return res.json({error:"already existed",status:"failure"});
+        } else {
+            person = new personModel({id,name,age,gender,photo,description});
+            await person.save();
+            return res.json({status:"completed",data:req.body});
+        }
+        // new personModel()  
     },
 
     get: (req,res) => {
