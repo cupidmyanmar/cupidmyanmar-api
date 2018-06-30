d2908e43800cc79eef2c4564bef12f9c285dae8e
nainglinaung <nainglinaung91@gmail.com>
2018-06-30 14:56:00 +0630

    typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var app = express();
-let bodyparser = require('bodyparser');
+let bodyparser = require('body-parser');
 
 
 // parse application/x-www-form-urlencoded
