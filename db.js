95e860708b9c820f65c1eae22f8f917317da4d3e
nainglinaung <nainglinaung91@gmail.com>
2018-06-30 14:09:30 +0630

    remove

--- db.js
+++ db.js
@@ -6,5 +6,5 @@ let dbuser = process.env.dbuser;
 let dbpassword = process.env.dbpassword;
 console.log(dbuser);
 console.log(dbpassword);
-mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds123981.mlab.com:23981/cupid`,{ useMongoClient: true});
+mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds123981.mlab.com:23981/cupid`);
 
