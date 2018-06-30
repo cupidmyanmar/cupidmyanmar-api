d43d4deb906a9120c3face32c9f013199eb2ce74
nainglinaung <nainglinaung91@gmail.com>
2018-06-30 15:08:21 +0630

    use promises

--- controllers/personController.js
+++ controllers/personController.js
@@ -14,14 +14,16 @@ let personController = {
         let data = req.body;
         // return res.json({status:"adssad",data});
         let {id,name,age,gender,photo,description} = req.body;
-        let person = await personModel.findOne({id});
-        if (person) {
-            return res.json({error:"already existed",status:"failure"});
-        } else {
-            person = new personModel({id,name,age,gender,photo,description});
-            await person.save();
-            return res.json({status:"completed",data:req.body});
-        }
+        return personModel.findOne({id}).then((person) => {
+                return res.json({hello:"hworld"});
+        });
+        // if (person) {
+        //     return res.json({error:"already existed",status:"failure"});
+        // } else {
+        //     person = new personModel({id,name,age,gender,photo,description});
+        //     await person.save();
+        //     return res.json({status:"completed",data:req.body});
+        // }
         // new personModel()  
     },
 
